4f2cd38271092d884b968d1bca7d16758562a2d0
JoshVizcaino <josh.vizcaino@gmail.com>
2020-04-13 00:43:31 +0100

    Heroes can fight and user can enter image to OCR manually

--- MotorSportREST/Assets/Client/Scripts/ClientExample.cs
+++ MotorSportREST/Assets/Client/Scripts/ClientExample.cs
@@ -4,12 +4,14 @@ using System.Text;
 using Client.Core;
 using Client.Core.Models;
 using TMPro;
+using UnityEngine.UI;
 using UnityEngine;
 
 namespace Client
 {
     public class ClientExample : MonoBehaviour
     {
+        [Header ("Links")]
         [SerializeField]
         private string baseUrl = "https://uksouth.api.cognitive.microsoft.com/vision/v2.0/ocr?language=unk&detectOrientation=true";
 
@@ -22,15 +24,62 @@ namespace Client
         [SerializeField]
         private string imageToOCR = "";
 
+        [Header ("UI")]
         [SerializeField]
         private TextMeshProUGUI header;
 
         [SerializeField]
         private TextMeshProUGUI wordsCapture;
 
+        [SerializeField]
+        private Button submitLinkButton;
+
+        [SerializeField]
+        private TMP_InputField linkInputField;
+
+
+
         void Start()
         {
+            header.text = "";
+            wordsCapture.text = "";
+            
+        }
 
+        void OnRequestComplete(Response response)
+        {
+            Debug.Log($"Status Code: {response.StatusCode}");
+            Debug.Log($"Data: {response.Data}");
+            Debug.Log($"Error: {response.Error}");
+
+            if (string.IsNullOrEmpty(response.Error) && !string.IsNullOrEmpty(response.Data))
+            {
+                AzureOCRResponse azureOCRResponse = JsonUtility.FromJson<AzureOCRResponse>(response.Data);
+
+                header.text = $"Orientation: {azureOCRResponse.orientation} Language: {azureOCRResponse.language} Text Angle: {azureOCRResponse.textAngle}";
+
+                string words = string.Empty;
+                foreach (var region in azureOCRResponse.regions)
+                {
+                    foreach (var line in region.lines)
+                    {
+                        foreach (var word in line.words)
+                        {
+                            words += word.text + "\n";
+                        }
+                    }
+                }
+                wordsCapture.text = words;
+            }
+            else
+            {
+                header.text = "PLEASE SUBMIT A VALID LINK TO AN IMAGE";
+                wordsCapture.text = "";
+            }
+        }
+
+        public void PostLink()
+        {
             // setup the request header
             RequestHeader clientSecurityHeader = new RequestHeader
             {
@@ -63,36 +112,24 @@ namespace Client
         }));
         }
 
-        void OnRequestComplete(Response response)
-        {
-            Debug.Log($"Status Code: {response.StatusCode}");
-            Debug.Log($"Data: {response.Data}");
-            Debug.Log($"Error: {response.Error}");
-
-            if (string.IsNullOrEmpty(response.Error) && !string.IsNullOrEmpty(response.Data))
-            {
-                AzureOCRResponse azureOCRResponse = JsonUtility.FromJson<AzureOCRResponse>(response.Data);
-
-                header.text = $"Orientation: {azureOCRResponse.orientation} Language: {azureOCRResponse.language} Text Angle: {azureOCRResponse.textAngle}";
-
-                string words = string.Empty;
-                foreach (var region in azureOCRResponse.regions)
-                {
-                    foreach (var line in region.lines)
-                    {
-                        foreach (var word in line.words)
-                        {
-                            words += word.text + "\n";
-                        }
-                    }
-                }
-                wordsCapture.text = words;
-            }
-        }
-
         public class ImageUrl
         {
             public string Url;
+        }
+
+        public void SubmitLink()
+        {
+            if (linkInputField.text != "")
+            {
+                imageToOCR = linkInputField.text;
+                PostLink();
+            }
+            else
+            {
+                header.text = "PLEASE SUBMIT A VALID LINK TO AN IMAGE";
+                wordsCapture.text = "";
+            }
+            
         }
     }
 }
--- MotorSportREST/Assets/Client/Scripts/HeroClientExample.cs
+++ MotorSportREST/Assets/Client/Scripts/HeroClientExample.cs
@@ -16,31 +16,61 @@ namespace Client
     
     public class HeroClientExample : MonoBehaviour
     {
-
+        [Header ("Link")]
         [SerializeField]
         private string baseUrl = "https://superheroapi.com/api/3991167154234731/";
 
-        [SerializeField]
-        private string clientId;
-
-        [SerializeField]
-        private string clientSecret;
-
+        [Header("UI")]
         [SerializeField]
         private TextMeshProUGUI heroNameText, heroIdText;
-
         [SerializeField]
         private TextMeshProUGUI[] heroSkillsTextArray;
-
         [SerializeField]
         private RawImage heroRawImage;
+        [SerializeField]
+        private TMP_InputField idInputField;
+        [SerializeField]
+        private Button searchButton;
+        [SerializeField]
+        private Button randomButton;
+        [SerializeField]
+        private TextMeshProUGUI heroNameText2, heroIdText2;
+        [SerializeField]
+        private TextMeshProUGUI[] heroSkillsTextArray2;
+        [SerializeField]
+        private RawImage heroRawImage2;
+        //[SerializeField]
+        //private TMP_InputField idInputField2;
+        [SerializeField]
+        private Button randomButton2;
+        [SerializeField]
+        private TextMeshProUGUI winnerText;
+        [SerializeField]
+        private Button fightButton;
 
         private string heroImageUrl;
+        private int count = 1;
+        private List<int> skillnumsList;
+        private int intel;
+        private int str;
+        private int speed;
+        private int dur;
+        private int pwr;
+        private int cmb;
+        private int intel2;
+        private int str2;
+        private int speed2;
+        private int dur2;
+        private int pwr2;
+        private int cmb2;
+        private int score1;
+        private int score2;
+        private bool b_hasScore = false;
+        private bool b_hasScore2 = false;
 
         void Start()
         {
-       
-
+            winnerText.text = "";
         }
 
         void OnRequestComplete(Response response)
@@ -49,8 +79,8 @@ namespace Client
             Debug.Log($"Status Code: {response.StatusCode}");
             Debug.Log($"Data: {response.Data}");
             Debug.Log($"Error: {response.Error}");
-
-            if (string.IsNullOrEmpty(response.Error) && !string.IsNullOrEmpty(response.Data))
+            
+            if (string.IsNullOrEmpty(response.Error) && !string.IsNullOrEmpty(response.Data) && count == 1)
             {
                 HeroAPIResponse heroAPIresponse = JsonUtility.FromJson<HeroAPIResponse>(response.Data);
                 heroNameText.text = $"Name: {heroAPIresponse.name}";
@@ -61,9 +91,35 @@ namespace Client
                 heroSkillsTextArray[3].text = $"Durability: {heroAPIresponse.durability}";
                 heroSkillsTextArray[4].text = $"Power: {heroAPIresponse.power}";
                 heroSkillsTextArray[5].text = $"Combat: {heroAPIresponse.combat}";
-
-
+                int.TryParse(heroAPIresponse.intelligence, out intel);
+                int.TryParse(heroAPIresponse.strength, out str);
+                int.TryParse(heroAPIresponse.speed, out speed);
+                int.TryParse(heroAPIresponse.durability, out dur);
+                int.TryParse(heroAPIresponse.power, out pwr);
+                int.TryParse(heroAPIresponse.combat, out cmb);
+                score1 = (intel + str + speed + dur + pwr + cmb);
+                b_hasScore = true;
             }
+            else if (string.IsNullOrEmpty(response.Error) && !string.IsNullOrEmpty(response.Data) && count == 2)
+            {   
+                HeroAPIResponse heroAPIresponse = JsonUtility.FromJson<HeroAPIResponse>(response.Data);
+                heroNameText2.text = $"Name: {heroAPIresponse.name}";
+                heroIdText2.text = $"ID: {heroAPIresponse.id}";
+                heroSkillsTextArray2[0].text = $"Intelligence: {heroAPIresponse.intelligence}";
+                heroSkillsTextArray2[1].text = $"Strength: {heroAPIresponse.strength}";
+                heroSkillsTextArray2[2].text = $"Speed: {heroAPIresponse.speed}";
+                heroSkillsTextArray2[3].text = $"Durability: {heroAPIresponse.durability}";
+                heroSkillsTextArray2[4].text = $"Power: {heroAPIresponse.power}";
+                heroSkillsTextArray2[5].text = $"Combat: {heroAPIresponse.combat}";
+                int.TryParse(heroAPIresponse.intelligence, out intel2);
+                int.TryParse(heroAPIresponse.strength, out str2);
+                int.TryParse(heroAPIresponse.speed, out speed2);
+                int.TryParse(heroAPIresponse.durability, out dur2);
+                int.TryParse(heroAPIresponse.power, out pwr2);
+                int.TryParse(heroAPIresponse.combat, out cmb2);
+                score2 = (intel2 + str2 + speed2 + dur2 + pwr2 + cmb2);
+                b_hasScore2 = true;
+            };
 
         }
 
@@ -73,8 +129,8 @@ namespace Client
             Debug.Log($"Status Code: {response.StatusCode}");
             Debug.Log($"Data: {response.Data}");
             Debug.Log($"Error: {response.Error}");
-
-            if (string.IsNullOrEmpty(response.Error) && !string.IsNullOrEmpty(response.Data))
+            
+            if (string.IsNullOrEmpty(response.Error) && !string.IsNullOrEmpty(response.Data) && count == 1)
             {
                 HeroAPIResponse heroAPIresponse = JsonUtility.FromJson<HeroAPIResponse>(response.Data);              
                 string url = heroAPIresponse.url;
@@ -92,6 +148,23 @@ namespace Client
                 heroRawImage.texture.filterMode = FilterMode.Point;
 
             }
+            else if (string.IsNullOrEmpty(response.Error) && !string.IsNullOrEmpty(response.Data) && count == 2)
+            {
+                HeroAPIResponse heroAPIresponse = JsonUtility.FromJson<HeroAPIResponse>(response.Data);
+                string url = heroAPIresponse.url;
+
+                UnityWebRequest r = UnityWebRequestTexture.GetTexture(url);
+                yield return r.SendWebRequest();
+
+                if (r.isNetworkError || r.isHttpError)
+                {
+                    Debug.Log(r.error);
+                    yield break;
+                }
+
+                heroRawImage2.texture = DownloadHandlerTexture.GetContent(r);
+                heroRawImage2.texture.filterMode = FilterMode.Point;
+            }
 
         }
 
@@ -103,33 +176,90 @@ namespace Client
 
             string heroURL2 = baseUrl + randomHeroIDindex.ToString() + "/image";
 
-
-            heroRawImage.texture = Texture2D.blackTexture;
-
-            heroNameText.text = "Loading...";
-
-            heroIdText.text = "#";
-
-            foreach (var heroSkillsText in heroSkillsTextArray)
+            if (count == 1)
             {
-                heroSkillsText.text = "";
+                heroRawImage.texture = Texture2D.blackTexture;
+
+                heroNameText.text = "Loading...";
+
+                heroIdText.text = "#";
+
+                foreach (var heroSkillsText in heroSkillsTextArray)
+                {
+                    heroSkillsText.text = "";
+                }
             }
+            else if (count == 2)
+            {
+                heroRawImage2.texture = Texture2D.blackTexture;
+
+                heroNameText2.text = "Loading...";
+
+                heroIdText2.text = "#";
+
+                foreach (var heroSkillsText2 in heroSkillsTextArray2)
+                {
+                    heroSkillsText2.text = "";
+                }
+            }
+          
 
             RequestHeader contentTypeHeader = new RequestHeader
             {
                 Key = "Content-Type",
                 Value = "application/json"
             };
+            
 
             // send a get request
             StartCoroutine(InterviewClient.Instance.HttpPost(heroURL1, JsonUtility.ToJson(heroURL1),(r) => OnRequestComplete(r)));
             StartCoroutine(InterviewClient.Instance.HttpPost(heroURL2,JsonUtility.ToJson(heroURL2), (r) => StartCoroutine(OnRequestComplete2(r))));
+            
         }
 
         public class HeroUrl
         {
             public string Url { get; set; }
         }
+
+        public void DisplayIDs()
+        {
+
+        }
+
+        public void WhichButton1()
+        {
+            count = 1;
+        }
+        public void WhichButton2()
+        {
+            count = 2;
+        }
+
+        public void Fight()
+        {
+            if (b_hasScore && b_hasScore2)
+            {
+                if (score1 > score2)
+                {
+                    winnerText.text = heroNameText.text + " IS THE WINNER!";
+                }
+                else if (score2 > score1)
+                {
+                    winnerText.text = heroNameText2.text + " IS THE WINNER!";
+                }
+                else
+                {
+                    winnerText.text = "DRAW";
+                }
+            }
+            else
+            {
+                winnerText.text = "Please select combatants!";
+            }
+            
+        }
+
 
     }
 }
